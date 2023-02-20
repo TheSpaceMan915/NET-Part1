817b58af68c7ff0c5d927001b03b49b4be7526f4
TheSpaceMan915 <97474520+TheSpaceMan915@users.noreply.github.com>
2023-02-20 20:08:55 +0000

    Lab8 v1.0
    Finished the task with integrals

---
+++ Lab8/Program.cs
@@ -0,0 +1,41 @@
+﻿using Lab8;
+
+Func<double, double> function = x => Math.Sin(x);
+int n0 = 1, n1 = 0;
+double start = 0, end = 7, middle = (end-start)/2;
+double eps = 0.0005, error = Double.MaxValue;
+
+int counter = 0;
+while (eps<error & counter!=15)
+{
+    // initialising n on the two intervals
+    counter++;
+    n0 *= 2;
+    n1 = n0 * 2;
+
+    // setting up threads
+    ThreadJob job1 = new ThreadJob(start, middle, n0, function);
+    ThreadJob job2 = new ThreadJob(middle, end, n1, function);
+    Thread thread1 = new Thread(new ThreadStart(job1.calculateSum));
+    Thread thread2 = new Thread(new ThreadStart(job2.calculateSum));
+    thread1.Start();
+    thread2.Start();
+    thread1.Join();
+    thread2.Join();
+
+    // getting sums on the intervals from the list
+    var list1 = job1.getListSums();
+    var list2 = job2.getListSums();
+    double sum1 = list1.Sum();
+    double sum2 = list2.Sum();
+    error = Math.Abs(sum2 - sum1);
+   
+    double res = sum1 + sum2;
+    Console.WriteLine("{0}. n0: {1}, n1: {2}, error: {3}, res: {4}", counter, n0, n1, error, res);
+
+    // cleaning the lists containing sums
+    list1.Clear();
+    list2.Clear();
+}
+
+Console.WriteLine("The job's done");
---
+++ Lab8/ThreadJob.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8
+{
+    internal class ThreadJob
+    {
+        private double a;
+        private double b;
+        private double n;
+        private Func<double, double> func;
+        private List<double> listSums;
+
+        public ThreadJob(double a, double b, double n, Func<double, double> func) {
+            this.a = a;
+            this.b = b;
+            this.n = n;
+            this.func = func;
+            listSums= new List<double>();
+        }
+
+        public List<double> getListSums() { return listSums; }
+
+/*        public static double calculateSum(double a, double b, int n)
+        {
+            double h = (b - a) / n;
+            double sum = 0;
+            a += 1 / 2 * h;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                double x = a + i * h;
+                sum += func(x);
+            }
+            return sum * h;
+        }*/
+
+        public void calculateSum()
+        {
+            double h = (this.b - this.a) / this.n;
+            double sum = 0;
+            this.a += 1/2 * h;
+
+            // calculating a sum on the [a,b] interval
+            for (int i = 0; i < this.n - 1; i++)
+            {
+                double x = this.a + i * h;
+                sum += func(x);
+            }
+
+            // saving the result in the list
+            double res = sum * h;
+            listSums.Add(res);
+        }
+    }
+}
